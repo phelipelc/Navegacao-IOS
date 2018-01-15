f907673b70149290d1a6e63151af236cc35a3937
phelipelc <phelipelcampos@hotmail.com>
2018-01-15 13:47:08 -0200

    navegar entre UIViews

---
+++ Navegacao-IOS/SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  Navegacao-IOS
+//
+//  Created by GVP on 15/01/18.
+//  Copyright © 2018 Phelipe Lopes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        dismiss(animated: true, completion:nil)
+            
+        }
+    
+    
+  
+
+}
--- Navegacao-IOS/ViewController.swift
+++ Navegacao-IOS/ViewController.swift
@@ -14,12 +14,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        performSegue(withIdentifier: "segue", sender: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func showSecondScreen(_ sender: Any) {
+        performSegue(withIdentifier: "segue", sender: nil)
+    }
+    @IBAction func undwindView1(segue: UIStoryboardSegue){
+        
+    }
 
 }
 
